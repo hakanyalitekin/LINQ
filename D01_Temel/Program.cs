c9ffb4dcb3592dd80b6b9ec649437378356e7e3a
Hakan <hakanyalitekin@gmail.com>
2020-03-20 13:01:37 +0300

    Add project files.

---
+++ D01_Temel/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D01_Temel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
