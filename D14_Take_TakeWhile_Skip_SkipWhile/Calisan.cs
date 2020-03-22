d2a47b3ab67bd8104648b5e79be7a87629f3ec42
Hakan <hakanyalitekin@gmail.com>
2020-03-22 22:27:33 +0300

    Ders 14 eklendi. Sayfalama örneği içerir.

---
+++ D14_Take_TakeWhile_Skip_SkipWhile/Calisan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D14_Take_TakeWhile_Skip_SkipWhile
+{
+    public class Calisan
+    {
+        public int ID { get; set; }
+        public string Ad { get; set; }
+        public string Departman { get; set; }
+        public static List<Calisan> GetAllEmployees()
+        {
+            return new List<Calisan>()
+            {
+                new Calisan() {ID = 1, Ad = "Pranaya", Departman = "IT" },
+                new Calisan() {ID = 2, Ad = "Priyanka", Departman = "IT" },
+                new Calisan() {ID = 3, Ad = "Preety", Departman = "IT" },
+                new Calisan() {ID = 4, Ad = "Sambit", Departman = "IT" },
+                new Calisan() {ID = 5, Ad = "Sudhanshu", Departman = "HR" },
+                new Calisan() {ID = 6, Ad = "santosh", Departman = "HR" },
+                new Calisan() {ID = 7, Ad = "Happy", Departman = "HR" },
+                new Calisan() {ID = 8, Ad = "Raja", Departman = "IT" },
+                new Calisan() {ID = 9, Ad = "Tarun", Departman = "IT" },
+                new Calisan() {ID = 10, Ad = "Bablu", Departman = "IT" },
+                new Calisan() {ID = 11, Ad = "Hina", Departman = "HR" },
+                new Calisan() {ID = 12, Ad = "Anurag", Departman = "HR" },
+                new Calisan() {ID = 13, Ad = "Dillip", Departman = "HR" },
+                new Calisan() {ID = 14, Ad = "Manoj", Departman = "HR" },
+                new Calisan() {ID = 15, Ad = "Lima", Departman = "IT" },
+                new Calisan() {ID = 16, Ad = "Sona", Departman = "IT" },
+            };
+        }
+    }
+
+}
---
+++ D14_Take_TakeWhile_Skip_SkipWhile/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D14_Take_TakeWhile_Skip_SkipWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Take
+             * Take() anahtar sözcüğü koleksiyon içerisinden listelenecek olan veri adedini belirtir.
+             * Sql Sorgu cümleciğinde Top() anahtar sözcüğüne eş değerdir.
+             */
+
+            var sayilar = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var sonuc1 = sayilar.Take(5);
+            foreach (var item in sonuc1)
+            {
+                Console.WriteLine("Take() -> " + item);
+            }
+
+            /*  TakeWhile   
+             *TakeWhile() anahtar sözcüğü Koleksiyon da bulunan verilerin, belirlenen koşul neticesinde listelenmesini sağlar.
+            */
+            var sonuc2 = sayilar.TakeWhile(x => x < 4);
+            foreach (var item in sonuc2)
+            {
+                Console.WriteLine("TakeWhile() -> " + item);
+            }
+
+            /*
+             * Skip
+             * Skip() anahtar sözcüğü koleksiyon içerisinde bulunan verilerin başlangıç index'ini belirler. Belirlenen index numarasından sonraki verileri getirir.
+             */
+            var sonuc3 = sayilar.Skip(5);
+            foreach (var item in sonuc3)
+            {
+                Console.WriteLine("Skip() -> " + item);
+            }
+
+            /* SkipWhile()
+             * Koleksiyonda bulunan verilerin ekrana getirilme index'inin belirlenen koşula göre çalıştıran metotdur.
+             */
+            var sonuc4 = sayilar.SkipWhile(x=>x < 1);
+            foreach (var item in sonuc4)
+            {
+                Console.WriteLine("SkipWhile() -> " + item);
+            }
+
+                                //******************* SAYFALAMA ÖRNEĞİ ******************
+            int SayfaBasinaKayitSayisi = 4;
+            int SayfaNumarasi = 0;
+            do
+            {
+                Console.WriteLine("1 - 4 arası sayfa numarası giriniz.");
+                if (int.TryParse(Console.ReadLine(), out SayfaNumarasi))
+                {
+                    if (SayfaNumarasi > 0 && SayfaNumarasi < 5)
+                    {
+                        var calisanlar = Calisan.GetAllEmployees().Skip((SayfaNumarasi - 1) * SayfaBasinaKayitSayisi).Take(SayfaBasinaKayitSayisi).ToList();
+                        Console.WriteLine();
+                        Console.WriteLine("Sayfa Numarası: " + SayfaNumarasi);
+                        foreach (var emp in calisanlar)
+                        {
+                            Console.WriteLine($"ID : {emp.ID}, İsim : {emp.Ad}, Departman : {emp.Departman}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Geçerli bir sayfa numarası giriniz.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Geçerli bir sayfa numarası giriniz.");
+                }
+            } while (true);
+        
+
+        }
+    }
+}
